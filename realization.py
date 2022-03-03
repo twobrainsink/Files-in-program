b9707dbd6deb806ebf69f928e95e4406a2f83f6a
Dmitry1515 <chuvilind05@mail.ru>
2022-03-03 16:38:38 +0500

    Create realization.py

---
+++ realization.py
@@ -0,0 +1,15 @@
+import base64
+
+def encode_to_base64(file:str) -> str:
+    with open(file, "rb") as file:
+        bytes_f = file.read()
+        output = base64.b64encode(bytes_f)
+    return output.decode()
+
+file = encode_to_base64("For Timur.7z")
+
+def decode_to_file(file_name:str, file:str) -> None:
+    with open(file_name, "wb") as f:
+        f.write(base64.b64decode(file))
+
+decode_to_file("Old Timur.7z", file)
